9dda7de05d7e313d459c058a00b1fd2aeabf4b81
Ginny Christman <ginny.christman@emory.edu>
2024-08-21 10:21:06 -0400

    Creating table 1, regression model, and a figure

--- Work Script.R
+++ Work Script.R
@@ -1,4 +1,24 @@
 library(tidyverse)
 library(here)
+library(gtsummary)
 
 athletes <- read_csv(here::here("Data","athlete_events.csv"))
+
+#gtsummary table of descriptive stats
+
+tbl_summary(
+	athletes,
+	by = Season,
+	include = c(Sex, Age, Height, Team, Sport))
+
+#Regression
+
+linear_model <- lm(Age ~ Height + Weight,
+									 data = athletes)
+
+tbl_regression(
+	linear_model,
+	intercept = TRUE)
+
+#Creating a figure
+hist(athletes$Age)
